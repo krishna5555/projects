a2099e9d2a518c05db5030747953a71231ffe00f
krishna5555 <akshathk55@gmail.com>
2020-11-14 13:38:18 +0530

    expense tracker initial commit

---
+++ expense/src/main/java/com/example/expense/model/Category.java
@@ -0,0 +1,36 @@
+package com.example.expense.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Entity
+@Table(name="category")
+public class Category {
+
+	@Id
+	private Long id;
+
+	//Travel, Grocery
+	private String name;
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ expense/src/main/java/com/example/expense/model/Expense.java
@@ -0,0 +1,72 @@
+package com.example.expense.model;
+
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="expense")
+public class Expense {
+
+	@Id
+	private Long id;
+
+	private Instant expenseDate;
+	
+	private String description;
+	
+	@ManyToOne
+	private Category category;
+	
+	@ManyToOne
+	private User user;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getExpenseDate() {
+		return expenseDate;
+	}
+
+	public void setExpenseDate(Instant expenseDate) {
+		this.expenseDate = expenseDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+}
